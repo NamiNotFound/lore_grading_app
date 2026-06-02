8cec3b8869bbd165c54a19d310b30f61e9968ac0
Just Nami <shinryujinim@gmail.com>
2026-06-02 01:23:43 -0800

    feat: configure Android Firebase, add Google Services plugin to Gradle

--- lore_grading_app/lib/firebase_options.dart
+++ lore_grading_app/lib/firebase_options.dart
@@ -8,22 +8,30 @@ class DefaultFirebaseOptions {
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        throw UnsupportedError('Android config not configured yet.');
+        return android;
       case TargetPlatform.iOS:
         throw UnsupportedError('iOS config not configured yet.');
       default:
-        // Default to web config for windows/web in development
         return web;
     }
   }
 
-  // Paste your Firebase Web Config values here
+  // Web config (placeholder)
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    apiKey: 'YOUR_WEB_API_KEY',
+    appId: 'YOUR_WEB_APP_ID',
+    messagingSenderId: '576163519216',
+    projectId: 'grading-app-ee894',
+    authDomain: 'grading-app-ee894.firebaseapp.com',
+    storageBucket: 'grading-app-ee894.firebasestorage.app',
+  );
+
+  // Android config from google-services.json
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyB74NiHi32KkSPGuHc6R3oyXmPJkiC9dvQ',
+    appId: '1:576163519216:android:30b65fff897fe7988d4035',
+    messagingSenderId: '576163519216',
+    projectId: 'grading-app-ee894',
+    storageBucket: 'grading-app-ee894.firebasestorage.app',
   );
 }
